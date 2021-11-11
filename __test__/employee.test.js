cb93626a76476347c34e077e884d19fb422aef91
Landrell Williams <dontess5@yahoo.com>
2021-11-11 12:15:21 -0500

    input first test logic, still don't understand completely, but here we go. let's see if it works

---
+++ __test__/employee.test.js
@@ -0,0 +1,25 @@
+//test logic
+const Employee = require('../lib/employee')
+describe('employee',() =>{
+    describe('name',()=>{
+    it('should return a name', ()=>{
+        const name = 'jordan'
+        const worker = new Employee('jordan','goat@yahoo.com','45')
+        expect(name).toEqual(worker.getName())
+    })
+    })
+    describe('email',()=>{
+        it('should return an email', ()=>{
+            const email = 'goat@yahoo.com'
+            const worker = new Employee('jordan','goat@yahoo.com','45')
+            expect(email).toEqual(worker.getEmail())
+        })
+        })
+    describe('id',()=>{
+        it('should return an id number', ()=>{
+            const id = '45'
+            const worker = new Employee('jordan','goat@yahoo.com','45')
+            expect(id).toEqual(worker.getEmail())
+        })
+        })    
+})
