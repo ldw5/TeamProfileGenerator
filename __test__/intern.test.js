7cc46c9fd33e1a1ceaa32f6282addd1c363107fd
Landrell Williams <dontess5@yahoo.com>
2021-11-22 20:05:52 -0500

    corrected test format to see if it works

--- __test__/intern.test.js
+++ __test__/intern.test.js
@@ -1,2 +1,20 @@
 //test logic
 const Intern = require('../lib/intern')
+
+test("Can set school via constructor", () => {
+    const testValue = "Duke";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+  });
+  
+  test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Foo", 1, "test@test.com", "Duke");
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+  test("Can get school via getSchool()", () => {
+    const testValue = "Duke";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+  });
